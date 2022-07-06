83a57d81db0da1812e9f265e606017aa6a8f07d6
krish-98 <muralikrishnanm98@gmail.com>
2022-07-06 17:48:14 +0530

    updating values in multiple places

---
+++ src/components/update/Update.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import Warning from "../warning/Warning"
+import "./update.css"
+
+import { remove, update } from "../../redux/userSlice"
+
+export default function Update() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const user = useSelector((state) => state.user)
+  const dispatch = useDispatch()
+
+  console.log(user)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(update({ name, email }))
+  }
+
+  const handleDelete = () => {
+    dispatch(remove())
+  }
+
+  return (
+    <div className="update">
+      <div className="updateWrapper">
+        <h3 className="updateTitle">Update Your Account</h3>
+        <Warning />
+        <button onClick={handleDelete} className="delete">
+          Delete Account
+        </button>
+
+        <div className="updateContainer">
+          <form>
+            <div className="formItem">
+              <label>Profile Picture</label>
+              <div className="profilePic">
+                <img
+                  className="avatar"
+                  src="https://images.pexels.com/photos/3024627/pexels-photo-3024627.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                  alt=""
+                />
+                <span className="change">Change</span>
+              </div>
+            </div>
+            <div className="formItem">
+              <label>Username</label>
+              <input
+                className="formInput"
+                type="text"
+                placeholder={user.name}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="formItem">
+              <label>Email</label>
+              <input
+                className="formInput"
+                type="text"
+                placeholder={user.email}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="formItem">
+              <label>Password</label>
+              <input className="formInput" type="password" />
+            </div>
+            <button className="updateButton" onClick={handleSubmit}>
+              Update
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
